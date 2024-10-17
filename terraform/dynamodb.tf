2fc8c8a6c547e6c30c9ce6016785ef81cd230de8
Amit Kumar <amitrke@gmail.com>
2024-10-17 13:11:16 -0700

    feat: Add new DynamoDB event table with TTL and global secondary index

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,4 +1,4 @@
-# Create a DynamoDB table for the application
+#Deprecated
 resource "aws_dynamodb_table" "this" {
   name           = var.app_name
   billing_mode   = "PAY_PER_REQUEST"
@@ -6,6 +6,39 @@ resource "aws_dynamodb_table" "this" {
   attribute {
     name = "id"
     type = "S"
+  }
+
+  deletion_protection_enabled = false
+}
+
+#Event Table
+resource "aws_dynamodb_table" "events" {
+  name           = "${var.app_name}-events"
+  billing_mode   = "PAY_PER_REQUEST"
+  
+  hash_key = "UserId"
+  range_key = "SortKey"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "SortKey"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "SortKeyIndex"
+    hash_key           = "SortKey"
+    range_key          = "UserId"
+    projection_type    = "KEYS_ONLY"
+  }
+
+  ttl {
+    attribute_name = "TTL"
+    enabled        = true
   }
 
   deletion_protection_enabled = false
